c3cb20b8207ac7e8347b5c9354592eca4a5a755a
mohammad2001ah <mah.2001.ah@gmail.com>
2025-03-10 06:39:34 +0300

    finally

--- ecommerce-store/src/App.js
+++ ecommerce-store/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar';
+import TextButtons from './component/Button';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <TextButtons/>
     </div>
   );
 }
--- ecommerce-store/src/component/Navbar.js
+++ ecommerce-store/src/component/Navbar.js
@@ -1,40 +1,71 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem, Box, InputBase } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [openMenu, setOpenMenu] = React.useState(false);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpenMenu(true);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    setOpenMenu(false);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Sotre</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={handleMenuClick}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Store
+        </Typography>
+
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Link</Button>
+          <Button
+            color="inherit"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleMenuClick}
+          >
+            Dropdown
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={openMenu}
+            onClose={handleMenuClose}
+            MenuListProps={{
+              'aria-labelledby': 'simple-menu',
+            }}
+          >
+            <MenuItem onClick={handleMenuClose}>Action</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Another action</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Something else here</MenuItem>
+          </Menu>
+          <Button color="inherit" disabled>Disabled</Button>
+        </Box>
+
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <InputBase
+            sx={{ color: 'inherit', ml: 2 }}
+            placeholder="Search"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+          <IconButton sx={{ color: 'inherit' }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
